97ab2f7e952b0ef5a95ff2f483a1c7b395e33942
Stefan Chao <Stefan.chao@kcl.ac.uk>
2018-12-26 22:25:02 +0000

    added add

--- index.js
+++ index.js
@@ -55,8 +55,7 @@ bot.on("message", async message => {
   if(message.content.startsWith(`${prefix}add`) && message.member.roles.find("name", "Art Maiderators")) {
     var k = message.content.match(/'(\w+)'/)[1];
     message.channel.send(k);
-    var s = message.content.substring(message.content.indexOf("\'", message.content.indexOf("\'") + 1), message.content.length);
-    var v = s.match(/'(\w+)'/)[1];
+    var v = message.content.match(/'(\w+)'/)[1];
     message.channel.send(v);
     hash[k] = v;
   }
