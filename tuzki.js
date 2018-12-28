365fcbe8fa66bbb61dfd50efde95e8ea8fd656fa
Stefan Chao <Stefan.chao@kcl.ac.uk>
2018-12-28 19:10:22 +0000

    added tuzki

--- tuzki.js
+++ tuzki.js
@@ -2,7 +2,7 @@ var update = function update(message, bot){
     message.channel.send(`${message.author}`, {
       file: 'https://media1.tenor.com/images/0ca7c15a15aa1a0a57fb0c482a2c220f/tenor.gif'
     });
-  });
+
 }
 
 module.exports.update = update;
